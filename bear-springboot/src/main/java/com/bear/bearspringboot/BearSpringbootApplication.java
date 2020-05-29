9c4936917603306a576a2d93aff77449ade8c1ef
gaoquansheng <962338171@qq.com>
2020-05-29 15:06:25 +0800

    解决vue页面刷新出错的问题

--- bear-springboot/src/main/java/com/bear/bearspringboot/BearSpringbootApplication.java
+++ bear-springboot/src/main/java/com/bear/bearspringboot/BearSpringbootApplication.java
@@ -3,6 +3,11 @@ package com.bear.bearspringboot;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 
 @SpringBootApplication
 @MapperScan("com.bear.bearspringboot.dao")
@@ -10,6 +15,16 @@ public class BearSpringbootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BearSpringbootApplication.class, args);
+    }
+
+    //解决vue项目刷新页面之后显示Whitelabel Error Page的问题
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableWebServerFactory> webServerFactoryCustomizer(){
+        System.out.println("我来刷新了");
+        return factory -> {
+            ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/index.html");
+            factory.addErrorPages(error404Page);
+        };
     }
 
 }
